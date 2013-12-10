9de10576629a1e184fde3e1258de1544951752e8
Marc <marc.stoecker@mindrevolution.com>
2013-12-10 12:37:59 +0100

    umbraco image block redone

--- App_Plugins/SirTrevor/blocks/umbraco-image.js
+++ App_Plugins/SirTrevor/blocks/umbraco-image.js
@@ -25,9 +25,6 @@ SirTrevor.Blocks.UmbracoImage = (function () {
 
         loadData: function (data) {
             block = this; // - unify context
-            //block.$editor.html($("<img>", { src: "/media/1008/ae8b059314150b6791414ac4856cfdfb3ce0e4d7ab87da0c2ce34f57a22d9205_big-thumb.jpg", class: "ust-previewimg" }));
-            console.log("data", data);
-            console.log("current block - load", block);
             block.setImage(data.id);
         },
 
@@ -36,7 +33,6 @@ SirTrevor.Blocks.UmbracoImage = (function () {
             if (typeof this.blockStorage.data.id === "undefined") {
                 // - preserve current scope for callback 'onMediaSelected'
                 block = this;
-                console.log("current block - render", block);
 
                 // - show media library and allow selection of an image/media (for now)
                 uDialogService.mediaPicker({ callback: this.onMediaSelected });
@@ -47,9 +43,8 @@ SirTrevor.Blocks.UmbracoImage = (function () {
             var media = {};
             media.id = e.id;
             media.contentTypeAlias = e.contentTypeAlias;
-            console.log("current block - mediaselect", block);
+
             // - save media data
-            console.log("setAndLoadData", media);
             block.setAndLoadData(media);
             block.ready();
         },
@@ -64,7 +59,6 @@ SirTrevor.Blocks.UmbracoImage = (function () {
                 var thumburl = uImageHelper.getThumbnailFromPath(uImageHelper.getImagePropertyValue({ imageModel: media }));
 
                 scope.$editor.html($("<img>", { src: thumburl, class: "ust-previewimg" }));
-                console.log("setimage", mediaid, media, thumburl, block);
             });
         }
 
