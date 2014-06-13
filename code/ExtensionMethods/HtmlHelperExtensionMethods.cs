33723a3f9b1efbcfe778a361e92d8abe706e8f8f
Anders Bjerner <abjerner@skybrud.dk>
2014-06-14 01:05:35 +0200

    Added some logic for rendering a partial view for the SirTrevor data
    Can be used like:
    @{
        SirTrevorDataValue value = Model.Content.GetPropertyValue<SirTrevorDataValue>("body");
        @Html.SirTrevor(value);
    }
    
    or simply:
    
    @Html.SirTrevor(Model.Content.GetPropertyValue("body"))

---
+++ code/ExtensionMethods/HtmlHelperExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace SirTrevor.ExtensionMethods {
+    
+    public static class HtmlHelperExtensionMethods {
+
+        public static MvcHtmlString SirTrevor(this HtmlHelper helper, object model) {
+            return SirTrevor(helper, "Default", model);
+        }
+        
+        public static MvcHtmlString SirTrevor(this HtmlHelper helper, string viewName, object model) {
+            return helper.Partial("SirTrevor/" + viewName, model);
+        }
+
+    }
+
+}
