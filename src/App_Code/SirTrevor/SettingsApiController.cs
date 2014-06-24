251b5c2d545b707e49bcb5d329e2eb26550427c6
Anders Bjerner <abjerner@skybrud.dk>
2014-06-24 23:59:56 +0200

    Testing + Changed the comment a bit
    Didn't really change much code in this commit, but did some further testing to make sure we cleanup nice in App_Code after moving the code to the "code" project

--- src/App_Code/SirTrevor/SettingsApiController.cs
+++ src/App_Code/SirTrevor/SettingsApiController.cs
@@ -1,6 +1,10 @@
 ﻿namespace SirTrevor.Settings.Controllers
 {
 
-    // Class moved to code project
+    // A bit of explanation: Since we now have more C# classes, I took the
+    // liberty of moving this class to the code project. However this file
+    // should still be here so it overwrites the old class that was here -
+    // this is to make sure that we avoid two Web API controllers with the
+    // same "signature".
 
 }
