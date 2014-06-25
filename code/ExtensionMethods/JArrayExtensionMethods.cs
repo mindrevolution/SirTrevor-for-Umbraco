1f631896c717876358c18bd99ae5001c33412506
Anders Bjerner <abjerner@skybrud.dk>
2014-06-26 00:33:18 +0200

    Added more extension methods for JSON.NET

---
+++ code/ExtensionMethods/JArrayExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace SirTrevor.ExtensionMethods {
+    
+    public static class JArrayExtensionMethods {
+
+        public static int GetInt32(this JArray array, int index) {
+            return GetTValue<int>(array, index);
+        }
+
+        public static long GetInt64(this JArray array, int index) {
+            return GetTValue<long>(array, index);
+        }
+
+        public static T GetTValue<T>(this JArray array, int index) {
+            if (array == null) return default(T);
+            JToken token = array[index];
+            return token == null ? default(T) : token.Value<T>();
+        }
+
+    }
+
+}
--- code/ExtensionMethods/JObjectExtensionMethods.cs
+++ code/ExtensionMethods/JObjectExtensionMethods.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json.Linq;
 
 namespace SirTrevor.ExtensionMethods {
 
@@ -10,8 +12,18 @@ namespace SirTrevor.ExtensionMethods {
             return token == null ? null : token.Value<string>();
         }
 
+        public static T GetString<T>(this JObject obj, string name, Func<string, T> func) {
+            if (obj == null || name == null) return default(T);
+            JToken token = obj[name];
+            return token == null ? default(T) : func(token.Value<string>());
+        }
+
         public static int GetInt32(this JObject obj, string name) {
             return GetTValue<int>(obj, name);
+        }
+
+        public static long GetInt64(this JObject obj, string name) {
+            return GetTValue<long>(obj, name);
         }
 
         public static T GetTValue<T>(this JObject obj, string name) {
@@ -20,6 +32,22 @@ namespace SirTrevor.ExtensionMethods {
             return token == null ? default(T) : token.Value<T>();
         }
 
-    }
+        public static T GetObject<T>(this JObject obj, string name, Func<JObject, T> func) {
+            if (obj == null || name == null) return default(T);
+            JObject token = obj[name] as JObject;
+            return token == null ? default(T) : func(token);
+        }
 
+        public static JArray GetArray(this JObject obj, string name) {
+            if (obj == null || name == null) return null;
+            return obj[name] as JArray;
+        }
+
+        public static T[] GetArray<T>(this JObject obj, string name, Func<JObject, T> func) {
+            if (obj == null || name == null) return null;
+            JArray array = obj[name] as JArray;
+            return array == null ? new T[0] : (from JObject entry in array select func(entry)).ToArray();
+        }
+
+    }
 }
